323a03a18d095eda8b8729aa8c5fc2b808fe4e81
Kin1599 <dkonopliannikov05@mail.ru>
2024-11-09 22:05:15 +0300

    refactoring code

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,12 +1,12 @@
 import MainPage from '../pages/MainPage/MainPage';
 import '../shared/styles/App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Header from '../widgets/Header/Header';
+// import Header from '../widgets/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/> */}
       <Routes>
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/" element={<MainPage/>} />
--- frontend/src/widgets/MapComponent/MapComponent.jsx
+++ frontend/src/widgets/MapComponent/MapComponent.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import cl from './MapComponent.module.scss';
 import { YMaps, Map, GeoObject } from '@pbe/react-yandex-maps';
 import danger from '../../shared/assets/danger.svg'
 import layer from '../../shared/assets/layer.svg'
+import magic from '../../shared/assets/magic.svg'
+import traffic from '../../shared/assets/traffic.svg'
 
 function MapComponent({ fetchHouseData, fetchMetroData, fetchStreetData, fetchBusData }) {
   const mapContainerRef = useRef(null);
   const [mapInstance, setMapInstance] = useState(null);
   const [isVisible, setIsVisible] = useState(false);
-  const [houses, setHouses] = useState([]);
-  const [streets, setStreets] = useState([]);
-  const [metroExits, setMetroExits] = useState([]);
-  const [busStations, setBusStations] = useState([]);
+  const [geoData, setGeoData] = useState({ houses: [], streets: [], metroExits: [], busStations: []})
   const [currentStage, setCurrentStage] = useState('stage1');
   const [isProblem, setIsProblem] = useState(false);
+  const [isTraffic, setIsTraffic] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const apiKey = process.env.REACT_APP_YMAPS_API_KEY;
 
@@ -32,22 +32,33 @@ function MapComponent({ fetchHouseData, fetchMetroData, fetchStreetData, fetchBu
     return () => observer.disconnect();
   }, []);
 
+  // Функция загрузки данных
+  const loadData = useCallback(
+    async (bounds, stage) => {
+      const [houseData, streetData, metroData, busData] = await Promise.all([
+        fetchHouseData(bounds, stage),
+        fetchStreetData(bounds, stage),
+        fetchMetroData(bounds),
+        fetchBusData(bounds),
+      ]);
+
+      setGeoData({
+        houses: houseData.features || [],
+        streets: streetData.features || [],
+        metroExits: metroData.features || [],
+        busStations: busData.features || [],
+      });
+    },
+    [fetchHouseData, fetchStreetData, fetchMetroData, fetchBusData]
+  );
+
   const handleBoundsChange = async () => {
     if (mapInstance){
       const bounds = mapInstance.getBounds();
-      
+
       if (bounds) {
         if(bounds){
-          const houseData = await fetchHouseData(bounds, currentStage);
-          const streetsData = await fetchStreetData(bounds, currentStage);
-          const metroData = await fetchMetroData(bounds);
-          const busData = await fetchBusData(bounds);
-
-          console.log(metroData);
-          setHouses(houseData.features);
-          setMetroExits(metroData.features);
-          setStreets(streetsData.features);
-          setBusStations(busData.features);
+          await loadData(bounds, currentStage);
         }
       }
     }
@@ -59,86 +70,67 @@ function MapComponent({ fetchHouseData, fetchMetroData, fetchStreetData, fetchBu
 
     if (mapInstance){
       const bounds = mapInstance.getBounds();
-
       if (bounds) {
-        const housesData = await fetchHouseData(bounds, stage);
-        const streetsData = await fetchStreetData(bounds, stage);
-        const metroData = await fetchMetroData(bounds, stage);
-        const busData = await fetchBusData(bounds, stage);
-
-        setHouses(housesData.features);
-        setStreets(streetsData.features);
-        setMetroExits(metroData.features);
-        setBusStations(busData.features);
+        await loadData(bounds, stage);
       }
     }
   };
 
+  const toggleState = (setter, value) => setter(!value);
+
+  //Функция для рендера GeoObject
+  const renderGeoObjects = (data, options, indexPrefix = '') =>
+    data.map((item, index) => (
+      <GeoObject
+        key={`${indexPrefix}-${index}`}
+        geometry={{type: item.geometry.type, coordinates: item.geometry.coordinates}}
+        properties={{balloonContent: item.properties.name}}
+        options={options}
+      />
+    ))
+
   return (
     <div className={cl.map} ref={mapContainerRef}>
-      <div className={cl.map__controls}>
-        <button className={cl.controls__problem} onClick={e => setIsProblem(!isProblem) }>
-          <img src={danger} alt="Показать проблемные зоны" className={`${cl.problem__img} ${isProblem ? cl.problem : ''}`}/>
+      <div className={`${cl.map__controls} ${cl.map__mainControls}`}>
+        <button className={cl.controls__button} onClick={() => toggleState(setIsTraffic, isTraffic)}>
+          <img src={traffic} alt="Показать нагруженность дорог" className={`${cl.button__img} ${isTraffic ? cl.traffic : ''}`}/>
         </button>
-        <button className={cl.controls__stage} onClick={() => setDropdownOpen(!dropdownOpen)}>
-          <img src={layer} alt="Выберите этап застройки" className={cl.stage__img}/>
+        <button className={cl.controls__button} onClick={() => toggleState(setIsProblem, isProblem)}>
+          <img src={danger} alt="Показать проблемные зоны" className={`${cl.button__img} ${isProblem ? cl.problem : ''}`}/>
+        </button>
+        <button className={cl.controls__button} onClick={() => setDropdownOpen(!dropdownOpen)}>
+          <img src={layer} alt="Выберите этап застройки" className={cl.button__img}/>
         </button>
         {
           dropdownOpen && (
             <div className={cl.stage__dropdownMenu}>
-              <button onClick={() => handleStageChange('stage1')} className={cl.dropdownMenu__item}>Этап 1</button>
-              <button onClick={() => handleStageChange('stage2')} className={cl.dropdownMenu__item}>Этап 2</button>
-              <button onClick={() => handleStageChange('stage3')} className={cl.dropdownMenu__item}>Этап 3</button>
+              {['stage1', 'stage2', 'stage3'].map((stage, index) => (
+                <button key={index} onClick={() => handleStageChange(stage)} className={cl.dropdownMenu__item}>
+                  Этап {index + 1}
+                </button>
+              ))}
             </div>
           )
         }
       </div>
+      <div className={`${cl.map__controls} ${cl.map__aiControls}`}>
+        <button className={cl.controls__button}>
+          <img src={magic} alt="Предложения что можно изменить с помощью фото" className={cl.button__img}/>
+        </button>
+      </div>
       {isVisible && (
         <YMaps query={{ apikey: apiKey }}>
           <Map
-            defaultState={{ center: [55.570395, 37.475495], zoom: 15 }}
+            defaultState={{ center: [55.55357355512132, 37.49579152990711], zoom: 15 }}
             width="100%"
             height="100dvh"
             instanceRef={map => setMapInstance(map)}
             onBoundsChange={handleBoundsChange}
           >
-           { houses && houses.map((house, index) => ( 
-              <GeoObject 
-                key={index} 
-                geometry={{ type: house.geometry.type, coordinates: house.geometry.coordinates }} 
-                properties={{ balloonContent: house.properties.name }} 
-                options={{ geodesic: true, strokeColor: "#F008", strokeWidth: 5, fillColor: "#F00833" }} 
-              /> 
-            ))}
-            { streets && streets.map((street, index) => ( 
-              <GeoObject 
-                key={index} 
-                geometry={{ type: street.geometry.type, coordinates: street.geometry.coordinates }} 
-                properties={{ balloonContent: street.properties.name }} 
-                options={{ strokeColor: "#0000ff", strokeWidth: 4 }} 
-              /> 
-            ))}
-            { metroExits && metroExits.map((metroExit, index) => ( 
-              <GeoObject
-                key={index} 
-                geometry={{type: metroExit.geometry.type, coordinates: metroExit.geometry.coordinates}} 
-                properties={{ 
-                  balloonContent: metroExit.properties.name,
-                  iconContent: metroExit.properties.id,
-                }} 
-                options={{ geodesic: true, iconColor: "#00ff00" }} 
-              /> 
-            ))}
-            { busStations && busStations.map((busStation, index) => ( 
-              <GeoObject
-                key={index} 
-                geometry={{type: busStation.geometry.type, coordinates: busStation.geometry.coordinates}} 
-                properties={{ 
-                  balloonContent: busStation.properties.name,
-                }} 
-                options={{ geodesic: true, iconColor: "#3b83ff" }} 
-              /> 
-            ))}
+            {renderGeoObjects(geoData.houses, { geodesic: true, strokeColor: "#F008", strokeWidth: 5, fillColor: "#F00833" }, 'house')}
+            {renderGeoObjects(geoData.streets, { strokeColor: "#0000ff", strokeWidth: 4 }, 'street')}
+            {renderGeoObjects(geoData.metroExits, { geodesic: true, iconColor: "#00ff00" }, 'metro')}
+            {renderGeoObjects(geoData.busStations, { geodesic: true, iconColor: "#3b83ff" }, 'bus')}
           </Map>
         </YMaps>
       )}
